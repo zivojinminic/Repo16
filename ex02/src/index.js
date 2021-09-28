9b7c674b607b6130384160e27c8a3e501363db2c
zivojinminic <zivojinminic05@gmail.com>
2021-09-28 12:49:20 +0200

    Getters and Setters

---
+++ ex02/src/index.js
@@ -0,0 +1,19 @@
+// Only change code below this line
+class Book  {
+    constructor(title) {
+        this._title = title;
+    }
+    get title() {
+        return this._title;
+    }
+    set title(newTitle) {
+        this._title = newTitle;
+    }
+}
+// Only change code above this line
+const littlePrince = new Book("Le Petit Prince");    //Change this line
+console.log(littlePrince.title);    //The Little Prince
+littlePrince.title = "Le Petit Prince" ;  //Change this line
+console.log(littlePrince.title);    //Le Petit Prince
+
+module.exports = Book;
